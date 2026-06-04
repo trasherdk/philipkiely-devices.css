8b274a7b238fb08b5b278597cbb72d07b5a7cd9b
trasherdk <trasherdk@gmail.com>
2026-06-04 08:13:52 +0700

    Add responsive font-size scaling with ResizeObserver

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-import './devices.css'
+const DEVICE_NATIVE_WIDTH = 375;
+
+const indicator = document.getElementById('font-size-indicator');
+const screen = document.querySelector('.marvel-device .screen');
+
+function updateDivisor(wrapper) {
+  const available = wrapper.clientWidth;
+  if (available > 0) {
+    wrapper.style.setProperty('--size-divisor', DEVICE_NATIVE_WIDTH / available);
+  }
+  if (indicator && screen) {
+    const computed = parseFloat(getComputedStyle(screen).fontSize).toFixed(1);
+    indicator.textContent = `font-size: ${computed}px`;
+  }
+}
+
+const wrapper = document.querySelector('.device-wrapper');
+if (wrapper) {
+  requestAnimationFrame(() => updateDivisor(wrapper));
+  new ResizeObserver(() => updateDivisor(wrapper)).observe(wrapper);
+}
